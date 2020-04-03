f02d8a4181f057787a56c52b436cbe6f657962eb
Gasan <gasan.alkadarskiy@gmail.com>
2020-04-03 20:44:03 +0300

    Added styles for pages

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Weather',
         name: 'Gasan'
     });
 });
